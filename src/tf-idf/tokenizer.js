f370d68a3485fcacb370b409f1eaa8eca7dde16c
Maksim Avilov <mavilov@hotmail.com>
2026-01-25 20:06:02 +0100

    Fix prettier

--- src/tf-idf/tokenizer.js
+++ src/tf-idf/tokenizer.js
@@ -14,22 +14,181 @@
  * Standard stop words (expanded set)
  */
 const DEFAULT_STOP_WORDS = new Set([
-    'a', 'about', 'above', 'after', 'again', 'against', 'all', 'am', 'an', 'and', 'any', 'are', 'aren\'t', 'as', 'at', 
-    'be', 'because', 'been', 'before', 'being', 'below', 'between', 'both', 'but', 'by', 
-    'can\'t', 'cannot', 'could', 'couldn\'t', 'did', 'didn\'t', 'do', 'does', 'doesn\'t', 'doing', 'don\'t', 'down', 'during', 
-    'each', 'few', 'for', 'from', 'further', 
-    'had', 'hadn\'t', 'has', 'hasn\'t', 'have', 'haven\'t', 'having', 'he', 'he\'d', 'he\'ll', 'he\'s', 'her', 'here', 'here\'s', 'hers', 'herself', 'him', 'himself', 'his', 'how', 'how\'s', 
-    'i', 'i\'d', 'i\'ll', 'i\'m', 'i\'ve', 'if', 'in', 'into', 'is', 'isn\'t', 'it', 'it\'s', 'its', 'itself', 
-    'let\'s', 'me', 'more', 'most', 'mustn\'t', 'my', 'myself', 
-    'no', 'nor', 'not', 
-    'of', 'off', 'on', 'once', 'only', 'or', 'other', 'ought', 'our', 'ours', 'ourselves', 'out', 'over', 'own', 
-    'same', 'shan\'t', 'she', 'she\'d', 'she\'ll', 'she\'s', 'should', 'shouldn\'t', 'so', 'some', 'such', 
-    'than', 'that', 'that\'s', 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'there', 'there\'s', 'these', 'they', 'they\'d', 'they\'ll', 'they\'re', 'they\'ve', 'this', 'those', 'through', 'to', 'too', 
-    'under', 'until', 'up', 'very', 
-    'was', 'wasn\'t', 'we', 'we\'d', 'we\'ll', 'we\'re', 'we\'ve', 'were', 'weren\'t', 'what', 'what\'s', 'when', 'when\'s', 'where', 'where\'s', 'which', 'while', 'who', 'who\'s', 'whom', 'why', 'why\'s', 'with', 'won\'t', 'would', 'wouldn\'t', 
-    'you', 'you\'d', 'you\'ll', 'you\'re', 'you\'ve', 'your', 'yours', 'yourself', 'yourselves'
-]);
-
+    'a',
+    'about',
+    'above',
+    'after',
+    'again',
+    'against',
+    'all',
+    'am',
+    'an',
+    'and',
+    'any',
+    'are',
+    "aren't",
+    'as',
+    'at',
+    'be',
+    'because',
+    'been',
+    'before',
+    'being',
+    'below',
+    'between',
+    'both',
+    'but',
+    'by',
+    "can't",
+    'cannot',
+    'could',
+    "couldn't",
+    'did',
+    "didn't",
+    'do',
+    'does',
+    "doesn't",
+    'doing',
+    "don't",
+    'down',
+    'during',
+    'each',
+    'few',
+    'for',
+    'from',
+    'further',
+    'had',
+    "hadn't",
+    'has',
+    "hasn't",
+    'have',
+    "haven't",
+    'having',
+    'he',
+    "he'd",
+    "he'll",
+    "he's",
+    'her',
+    'here',
+    "here's",
+    'hers',
+    'herself',
+    'him',
+    'himself',
+    'his',
+    'how',
+    "how's",
+    'i',
+    "i'd",
+    "i'll",
+    "i'm",
+    "i've",
+    'if',
+    'in',
+    'into',
+    'is',
+    "isn't",
+    'it',
+    "it's",
+    'its',
+    'itself',
+    "let's",
+    'me',
+    'more',
+    'most',
+    "mustn't",
+    'my',
+    'myself',
+    'no',
+    'nor',
+    'not',
+    'of',
+    'off',
+    'on',
+    'once',
+    'only',
+    'or',
+    'other',
+    'ought',
+    'our',
+    'ours',
+    'ourselves',
+    'out',
+    'over',
+    'own',
+    'same',
+    "shan't",
+    'she',
+    "she'd",
+    "she'll",
+    "she's",
+    'should',
+    "shouldn't",
+    'so',
+    'some',
+    'such',
+    'than',
+    'that',
+    "that's",
+    'the',
+    'their',
+    'theirs',
+    'them',
+    'themselves',
+    'then',
+    'there',
+    "there's",
+    'these',
+    'they',
+    "they'd",
+    "they'll",
+    "they're",
+    "they've",
+    'this',
+    'those',
+    'through',
+    'to',
+    'too',
+    'under',
+    'until',
+    'up',
+    'very',
+    'was',
+    "wasn't",
+    'we',
+    "we'd",
+    "we'll",
+    "we're",
+    "we've",
+    'were',
+    "weren't",
+    'what',
+    "what's",
+    'when',
+    "when's",
+    'where',
+    "where's",
+    'which',
+    'while',
+    'who',
+    "who's",
+    'whom',
+    'why',
+    "why's",
+    'with',
+    "won't",
+    'would',
+    "wouldn't",
+    'you',
+    "you'd",
+    "you'll",
+    "you're",
+    "you've",
+    'your',
+    'yours',
+    'yourself',
+    'yourselves',
+])
 
 /**
  * Simple, non-aggressive stemmer.
@@ -39,24 +198,27 @@ const DEFAULT_STOP_WORDS = new Set([
  */
 const _stem = (token) => {
     if (token.length <= 3) {
-        return token;
+        return token
     }
 
     if (token.endsWith('ing')) {
-        const stem = token.slice(0, -3);
-        if (stem.length > 1 && stem[stem.length - 1] === stem[stem.length - 2] && stem[stem.length - 1] !== 's') {
-            return stem.slice(0, -1);
+        const stem = token.slice(0, -3)
+        if (
+            stem.length > 1 &&
+            stem[stem.length - 1] === stem[stem.length - 2] &&
+            stem[stem.length - 1] !== 's'
+        ) {
+            return stem.slice(0, -1)
         }
-        return stem;
+        return stem
     } else if (token.endsWith('ies')) {
-        return token.slice(0, -3) + 'y';
+        return token.slice(0, -3) + 'y'
     } else if (token.endsWith('s') && !token.endsWith('ss')) {
-        return token.slice(0, -1);
+        return token.slice(0, -1)
     }
 
-    return token;
+    return token
 }
-
 
 /**
  * Simple, non-aggressive tokenizer. Converts text to lowercase and splits by non-word characters.
@@ -87,7 +249,7 @@ export const tokenize = (text, options = {}) => {
 
     for (let t of rawTokens) {
         // remove leading/trailing dots
-        t = t.replace(/(^\.+|\.+$)/g, '');
+        t = t.replace(/(^\.+|\.+$)/g, '')
 
         // Skip empty strings
         if (t.length === 0) continue
@@ -95,7 +257,7 @@ export const tokenize = (text, options = {}) => {
         // Skip stop words
         if (stopWords.has(t)) continue
 
-        let token = useStemming ? _stem(t) : t;
+        let token = useStemming ? _stem(t) : t
 
         results.push(token)
     }
